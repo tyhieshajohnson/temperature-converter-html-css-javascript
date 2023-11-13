f93d83063fd499cac4d7371033720e8f5a9b84f9
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-13 21:40:27 +0200

    cssAdd

--- JS/script.js
+++ JS/script.js
@@ -2,12 +2,12 @@ let convertButton = document.querySelector("#convertButton")
 
 function convert(){
     let celsius = document.querySelector("#celsius")
-    let farenheit = document.querySelector('#farenheit')
+    let fahrenheit = document.querySelector('#fahrenheit')
     if (+celsius.value > 0){
         let converted = +celsius.value * 9/5 + 32
-        farenheit.value = converted
-    }else if (+farenheit.value > 0) {
-        let converted = (+farenheit.value - 32) * 5/9
+        fahrenheit.value = converted
+    }else if (+fahrenheit.value > 0) {
+        let converted = (+fahrenheit.value - 32) * 5/9
         celsius.value = converted
     }else {
         alert("Value Missing")
